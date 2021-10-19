5c673609b7828e098dcf7911de46895031efcb3d
PARTHIPAN KANNAN <66922169+DONTSAYCANT@users.noreply.github.com>
2021-10-19 23:14:48 +0530

    Add files via upload

---
+++ library_explain_1.py
@@ -0,0 +1,10 @@
+def gokul_func(val1,val2):
+    val3 = val1 + val2[::-1] + 3
+    return val3
+
+def ravi_func(val1,val2,val3):
+    val4 = val1 + val2 + val3[::2]
+    return val4
+
+
+print("hello package")
